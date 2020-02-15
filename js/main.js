799d9b1ab9ac76d73c332ff4a32e4a0adbec7ac5
GiandoDev <giandomenicoriceputi@gmail.com>
2020-02-15 14:50:47 +0100

    first commit-pt-2

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const accordionContainer = document.querySelector(".accordion-container");
+
+accordionContainer.addEventListener("click", event => {
+  const accordionHeader = event.target.closest(".accordion__header");
+
+  if (accordionHeader) {
+    const accordion = accordionHeader.parentElement;
+    accordion.classList.toggle("is-open");
+  }
+});
